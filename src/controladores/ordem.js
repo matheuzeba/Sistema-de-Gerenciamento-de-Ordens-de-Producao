a138a3642181926c62597a32e839c89b1cf90f9a
matheuzeba <tavaresviana82@gmail.com>
2023-10-14 15:07:33 -0300

    criado controlador para criar ordem de fabricacao

--- src/controladores/ordem.js
+++ src/controladores/ordem.js
@@ -1,10 +1,21 @@
 const  pool = require('../bancoDeDados/conexao');
 
 const ordemDeFabricacao = async(req, res) => {
-    const { cliente, produto, quantidade, material, data_de_entrega } = req.body
-    const { rows } = await pool.query('select * from materiais');
-
-    res.json(rows);
+    try {
+        const { cliente, produto, quantidade, material, data_de_entrega } = req.body
+        const { rows } = await pool.query(
+            `
+            insert into ordens(cliente, produto, quantidade, material, data_de_entrega, concluida)
+            values
+            ($1, $2, $3, $4, $5, $6) returning *;
+            `, [cliente, produto, quantidade, material, data_de_entrega, false]
+        );
+    
+        res.status(201).json(rows[0]);
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json(error);
+    }
 }
 
 module.exports = {
